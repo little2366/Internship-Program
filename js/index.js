f25fe780fbd1a197791e3122a0349ce693c6801a
little2366 <466707620@qq.com>
2017-07-19 21:02:31 +0800

    7/19内容添加

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ $(".tabbtn-item").on("click",function(){
     $(this).addClass("current");
     $(this).siblings(".tabbtn-item").removeClass("current");
     var string=$(this).find("span").text();
-    if(string == '发布需求'){
+    if(string == '发布加工需求'){
        $(".con1").css("display","block");
        $(".con2").css("display","none");
     }else{
@@ -147,35 +147,246 @@ $("#imgPicker").on("change","input[type='file']",function() {
 $('#datetimepicker').datetimepicker();
 
 
-/*接单类型点击事件*/
-function jdlxOnclick(jdlxObj){
-    if(jdlxObj == ''){
-        if ($(".jdlx").attr("value") == "jdtypel") {
-            $(".jdlx input").stop().animate({"left": "133px"}, 500),
-            $(".jdlx").attr("value", "jdtyper")
-            $(".jdlx input").attr("value", "不含税")
-            $(".jdtype h3").text("不含税报价：不要求报价供方提供发票。")
-            $("input[name=enquiryType][value=jdtyper]").attr("checked","checked");//选中
+/*选择付款的方式*/
+$(".payType").on("click",function(){
+
+    if($(this).hasClass("typeContC")){
+       $(this).siblings(".payType").removeClass("typeContC");
+   }
+});
+
+
+/*是否含税点击事件*/
+function hslxOnclick(hslxObj){
+    if(hslxObj == ''){
+        if ($(".hslx").attr("value") == "hstypel") {
+            $(".hslx input").stop().animate({"left": "126px"}, 500),
+            $(".hslx").attr("value", "hstyper")
+            $(".hslx input").attr("value", "不含税")
+            $(".hstype h3").text("不含税报价：不要求报价供方提供发票。")
+            $("input[name=enquiryType][value=hstyper]").attr("checked","checked");//选中
         } else { 
-            $(".jdlx input").stop().animate({"left": "0"}, 400),
-            $(".jdlx").attr("value", "jdtypel")
-            $(".jdlx input").attr("value", "含税")
-            $(".jdtype h3").text("含税报价：要求报价供方提供发票。")
-            $("input[name=enquiryType][value=jdtypel]").attr("checked","checked");//选中
+            $(".hslx input").stop().animate({"left": "0"}, 400),
+            $(".hslx").attr("value", "hstypel")
+            $(".hslx input").attr("value", "含税")
+            $(".hstype h3").text("含税报价：要求报价供方提供发票。")
+            $("input[name=enquiryType][value=hstypel]").attr("checked","checked");//选中
         }
     }else{
-        if(jdlxObj == 'jdtypel'){
-            $(".jdlx input").stop().animate({"left": "0"}, 400),
-            $(".jdlx").attr("value", "jdtypel")
-            $(".jdlx input").attr("value", "含税")
-            $(".jdtype h3").text("含税报价：要求报价供方提供发票。")
-            $("input[name=enquiryType][value=jdtypel]").attr("checked","checked");//选中
-        }else if(jdlxObj == 'jdtyper'){
-            $(".jdlx input").stop().animate({"left": "133px"}, 500),
-            $(".jdlx").attr("value", "jdtyper")
-            $(".jdlx input").attr("value", "不含税")
-            $(".jdtype h3").text("不含税报价：不要求报价供方提供发票。")
-            $("input[name=enquiryType][value=jdtyper]").attr("checked","checked");//选中
+        if(hslxObj == 'hstypel'){
+            $(".hslx input").stop().animate({"left": "0"}, 400),
+            $(".hslx").attr("value", "hstypel")
+            $(".hslx input").attr("value", "含税")
+            $(".hstype h3").text("含税报价：要求报价供方提供发票。")
+            $("input[name=enquiryType][value=hstypel]").attr("checked","checked");//选中
+        }else if(hslxObj == 'hstyper'){
+            $(".hslx input").stop().animate({"left": "126px"}, 500),
+            $(".hslx").attr("value", "hstyper")
+            $(".hslx input").attr("value", "不含税")
+            $(".hstype h3").text("不含税报价：不要求报价供方提供发票。")
+            $("input[name=enquiryType][value=hstyper]").attr("checked","checked");//选中
         }
     }
 }
+
+/*是否缴纳滞纳金点击事件*/
+function znjlxOnclick(znjlxObj){
+    if(znjlxObj == ''){
+        if ($(".znjlx").attr("value") == "znjtypel") {
+            $(".znjlx input").stop().animate({"left": "126px"}, 500),
+            $(".znjlx").attr("value", "znjtyper")
+            $(".znjlx input").attr("value", "不缴纳滞纳金")
+            $(".znjtype h3").text("不缴纳滞纳金：发单方对接单方逾期申报税款或未按规定期限缴纳税款的行为不将给予的经济制裁。")
+            $("input[name=enquiryType][value=znjtyper]").attr("checked","checked");//选中
+        } else { 
+            $(".znjlx input").stop().animate({"left": "0"}, 400),
+            $(".znjlx").attr("value", "znjtypel")
+            $(".znjlx input").attr("value", "缴纳滞纳金")
+            $(".znjtype h3").text("缴纳滞纳金：发单方对接单方逾期申报税款或未按规定期限缴纳税款的行为给予的经济制裁。")
+            $("input[name=enquiryType][value=znjtypel]").attr("checked","checked");//选中
+        }
+    }else{
+        if(znjlxObj == 'znjtypel'){
+            $(".znjlx input").stop().animate({"left": "0"}, 400),
+            $(".znjlx").attr("value", "znjtypel")
+            $(".znjlx input").attr("value", "缴纳滞纳金")
+            $(".znjtype h3").text("缴纳滞纳金：发单方对接单方逾期申报税款或未按规定期限缴纳税款的行为给予的经济制裁。")
+            $("input[name=enquiryType][value=znjtypel]").attr("checked","checked");//选中
+        }else if(znjlxObj == 'znjtyper'){
+            $(".znjlx input").stop().animate({"left": "126px"}, 500),
+            $(".znjlx").attr("value", "znjtyper")
+            $(".znjlx input").attr("value", "不缴纳滞纳金")
+            $(".znjtype h3").text("不缴纳滞纳金：发单方对接单方逾期申报税款或未按规定期限缴纳税款的行为不将给予的经济制裁。")
+            $("input[name=enquiryType][value=znjtyper]").attr("checked","checked");//选中
+        }
+    }
+}
+
+/*是否自选商家点击事件*/
+function sjlxOnclick(sjlxObj){
+    if(sjlxObj == ''){
+        if ($(".sjlx").attr("value") == "sjtypel") {
+            $(".sjlx input").stop().animate({"left": "126px"}, 500),
+            $(".sjlx").attr("value", "sjtyper")
+            $(".sjlx input").attr("value", "不自选商家")
+            $(".sjtype h3").text("不自选商家：系统给发单方匹配合适的接单方。")
+            $("input[name=enquiryType][value=sjtyper]").attr("checked","checked");//选中
+        } else { 
+            $(".sjlx input").stop().animate({"left": "0"}, 400),
+            $(".sjlx").attr("value", "sjtypel")
+            $(".znjlx input").attr("value", "自选商家")
+            $(".sjtype h3").text("自选商家：发单方自行选择合适的接单方。")
+            $("input[name=enquiryType][value=sjtypel]").attr("checked","checked");//选中
+        }
+    }else{
+        if(sjlxObj == 'sjtypel'){
+            $(".sjlx input").stop().animate({"left": "0"}, 400),
+            $(".sjlx").attr("value", "sjtypel")
+            $(".sjlx input").attr("value", "自选商家")
+            $(".sjtype h3").text("自选商家：发单方自行选择合适的接单方。")
+            $("input[name=enquiryType][value=sjtypel]").attr("checked","checked");//选中
+        }else if(sjlxObj == 'sjtyper'){
+            $(".sjlx input").stop().animate({"left": "126px"}, 500),
+            $(".sjlx").attr("value", "sjtyper")
+            $(".sjlx input").attr("value", "不自选商家")
+            $(".sjtype h3").text("不自选商家：系统给发单方匹配合适的接单方。")
+            $("input[name=enquiryType][value=sjtyper]").attr("checked","checked");//选中
+        }
+    }
+}
+
+/*是否包含质量违约金点击事件*/
+function wyjlxOnclick(wyjlxObj){
+    if(wyjlxObj == ''){
+        if ($(".wyjlx").attr("value") == "wyjtypel") {
+            $(".wyjlx input").stop().animate({"left": "126px"}, 500),
+            $(".wyjlx").attr("value", "wyjtyper")
+            $(".wyjlx input").attr("value", "不含违约金")
+            $(".wyjtype h3").text("不含违约金：发单方不会对接单方的违约行为进行扣款处罚。")
+            $("input[name=enquiryType][value=wyjtyper]").attr("checked","checked");//选中
+        } else { 
+            $(".wyjlx input").stop().animate({"left": "0"}, 400),
+            $(".wyjlx").attr("value", "wyjtypel")
+            $(".wyjlx input").attr("value", "含违约金")
+            $(".wyjtype h3").text("含违约金：发单方会对接单方的违约行为进行扣款处罚。")
+            $("input[name=enquiryType][value=wyjtypel]").attr("checked","checked");//选中
+        }
+    }else{
+        if(wyjlxObj == 'wyjtypel'){
+            $(".wyjlx input").stop().animate({"left": "0"}, 400),
+            $(".wyjlx").attr("value", "wyjtypel")
+            $(".wyjlx input").attr("value", "含违约金")
+            $(".wyjtype h3").text("含违约金：发单方会对接单方的违约行为进行扣款处罚。")
+            $("input[name=enquiryType][value=wyjtypel]").attr("checked","checked");//选中
+        }else if(wyjlxObj == 'wyjtyper'){
+            $(".wyjlx input").stop().animate({"left": "126px"}, 500),
+            $(".wyjlx").attr("value", "wyjtyper")
+            $(".wyjlx input").attr("value", "不含违约金")
+            $(".wyjtype h3").text("不含违约金：发单方不会对接单方的违约行为进行扣款处罚。")
+            $("input[name=enquiryType][value=znjtyper]").attr("checked","checked");//选中
+        }
+    }
+}
+
+
+/*图形验证码*/
+var verifyCode = new GVerify("v_container");
+/*验证是否正确*/
+/*document.getElementById("my_button").onclick = function(){
+    var res = verifyCode.validate(document.getElementById("code_input").value);
+    if(res){
+        alert("验证正确");
+    }else{
+        alert("验证码错误");
+    }
+}*/
+
+
+/*获取手机验证码*/
+$(function (){
+    //获取手机验证码
+    $("#j_getVerifyCode").on("click",getVerifyCode({
+        callBack: function (){//按钮点击后的回调函数，-----必须-----
+            //在这里你还是可以对你的按钮进行操作
+            //console.log(this);
+            alert("验证码发送成功");
+        },
+        time: 60,//定时时间，以秒为单位，默认60秒
+        getCurrentTime: function (time){//获取倒计时当前时间
+            //console.log(time);
+            //console.log(this);//这里的这个this执行按钮
+            //console.log("=================================");
+        },
+        isPhone: true,//是否为发送手机验证码，如果是则会验证手机号格式，-----必须-----
+        getPhone: function (){//获取手机号，此处一定要return
+            return $("#j_phone").val();
+        },
+        //phoneReg: /^1[34578]\d{9}$/,//手机号验证正则
+        phoneCallBack: function (){//当手机号有误时的回调，默认会中止操作
+            alert("您输入的手机号有误");
+        },
+        timeIsUpText: "重新发送",//倒计时时间到了后按钮所显示文字
+        timeRunnigText: "s后重新发送",//倒计时时间正在走的时候按钮所显示文字。默认为"xxs后重新获取"
+        unabledClass: "unlabed"//按钮不能用的样式，即点击按钮后的样式
+    }));
+    
+});
+
+;(function (window,document){
+    function getVerifyCode(options) {
+        var fn = arguments.callee;
+        return function() {
+            clearInterval(timer);
+            if(!(options && Object.prototype.toString.call(options.callBack) == "[object Function]")) {
+                throw new Error("Error");
+            }
+            var that = $(this);
+            if(options.isPhone){
+                var phone = options.getPhone(),
+                    reg = options.phoneReg || /^1[3|4|5|7|8][0-9]\d{8}$/;
+                if(!reg.test(phone)) {
+                    options.phoneCallBack && options.phoneCallBack.call(that,phone);
+                    return;
+                }
+            }
+            
+            var timer = null,
+                time = options.time || 60,
+                count = 0,
+                interval = 1000,
+                start = new Date().getTime(),
+                targetTime = time * 1000,
+                unabledClass = options.unabledClass,
+                timeIsUpText = options.timeIsUpText,
+                timeRunnigText = options.timeRunnigText;
+            that.off("click");
+            that.addClass(unabledClass);
+            timer = setTimeout(function() {
+                var wucha = 0,
+                    nextRunTime = interval,
+                    currentFn = arguments.callee;
+                count ++;
+                wucha = new Date().getTime() - (start + count * interval);
+                wucha = (wucha <= 0) ? 0 : wucha;
+                nextRunTime = interval - wucha;
+                nextRunTime = (nextRunTime <= 0) ? 0 : nextRunTime
+                if((targetTime -= interval) <= 0){
+                    clearTimeout(timer);
+                    /*time = 60;*/
+                    that.html(timeIsUpText).removeClass(unabledClass);
+                    that.on("click", fn(options));
+                }else{
+                    time--;
+                    that.html(time + timeRunnigText);
+                    if(options.getCurrentTime && (Object.prototype.toString.call(options.getCurrentTime) == "[object Function]")){
+                        options.getCurrentTime.call(that,time);
+                    }
+                    timer = setTimeout(currentFn,nextRunTime);
+                }
+            }, interval);
+            options.callBack.call(that);
+        }
+    }
+    window.getVerifyCode = getVerifyCode;
+})(window,document);
+
