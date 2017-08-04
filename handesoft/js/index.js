eebb606bb50ceea11dcae9c841b4c3f76b199a53
little2366 <466707620@qq.com>
2017-08-04 17:25:55 +0800

    8/4内容

--- handesoft/js/index.js
+++ handesoft/js/index.js
@@ -30,7 +30,7 @@ function detectmob() {
     }  
 } 
 
-$(".btn").click(function(){
+$(".slideDown").click(function(){
     var t = $(window).scrollTop();
     var q1_height = $(".q1").height(); 
     if(!detectmob()){
@@ -42,9 +42,18 @@ $(".btn").click(function(){
     $('body').animate({'scrollTop':t+q1_height},600);
 });
 
+
+$(".q2Img").mouseenter(function(){
+    $(this).animate({'width':70,"height":70},300);
+});
+$(".q2Img").mouseleave(function(){
+    $(this).animate({'width':60,"height":60},300);
+});
+
+
 $("body").scroll(function(){
     var t = $(window).scrollTop();
-    var q1_height = $(".q1").height(); 
+    var q1_height = $(".q1").height();  
     if(!detectmob()){
        q1_height -= 80;
     }
@@ -52,19 +61,42 @@ $("body").scroll(function(){
        q1_height += 60;
     }
     $('body').animate({'scrollTop':t+q1_height},600);
+
 });
 
 
 $(function(){
     $(window).scroll(function(){
-    	var q1_height = $(".q1").offset().top;
+    	var q1_height = $(".q1").height();
+        q1_height -= 120;
+        var q2_height = $(".q2").height();
+        /*var q2_scroll = q1_height + q2_height;*/
         var this_scrollTop = $(this).scrollTop();
         /*根据滑动条位置动画*/
         if(this_scrollTop>q1_height ){
-            $(".q2Container").addClass("active");
+            $(".q2Container").addClass("q2Active");
         };
     });
 });
+
+$(function(){
+    $(window).scroll(function(){
+        var q1_height = $(".q1").offset().top;
+        var q2_height = $(".q2").height(); 
+        var q3_height = $(".q3").height();  
+        var q4_height = $(".q4").height();  
+        var q4_scroll = q1_height + q2_height + q3_height + q4_height;
+        var this_scrollTop = $(this).scrollTop();
+        /*根据滑动条位置动画*/
+        if(this_scrollTop > q4_scroll ){
+            $(".q4").addClass("q4Active");
+            $(".q4Img").addClass("q4ImgActive");
+            $(".q4HeadTitle").addClass("q4HeadTitleActive");
+        };
+    });
+});
+
+
 
 $(".fa-bars").on("click",function(){
     var display =$('.menu').css('display');
