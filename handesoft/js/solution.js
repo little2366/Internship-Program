d31f9671178fd40b6728dc924dad7e3c2b25df8b
little2366 <466707620@qq.com>
2017-08-09 16:11:16 +0800

    8/9

--- handesoft/js/solution.js
+++ handesoft/js/solution.js
@@ -1,58 +1,58 @@
 /*给div添加边框*/
-$(".q2Items").mouseenter(function(){
-    $(this).find(".q2ItemBox").css("border","1px solid #69c2ee");
+$(".q3Items").mouseenter(function(){
+    $(this).find(".q3ItemBox").css("border","1px solid #69c2ee");
 });
-$(".q2Items").mouseleave(function(){
-    $(this).find(".q2ItemBox").css("border","1px solid #fff");
+$(".q3Items").mouseleave(function(){
+    $(this).find(".q3ItemBox").css("border","1px solid #fff");
 });
 
 
 /*修改图片*/
-$("#q2Item1").mouseenter(function(){
-    document.getElementById("q2Img1").src="../img/solution/q2_9.png";
+$("#q3Item1").mouseenter(function(){
+    document.getElementById("q3Img1").src="../img/solution/q3_9.png";
 });
-$("#q2Item1").mouseleave(function(){
-    document.getElementById("q2Img1").src="../img/solution/q2_1.png";
+$("#q3Item1").mouseleave(function(){
+    document.getElementById("q3Img1").src="../img/solution/q3_1.png";
 });
-$("#q2Item2").mouseenter(function(){
-    document.getElementById("q2Img2").src="../img/solution/q2_10.png";
+$("#q3Item2").mouseenter(function(){
+    document.getElementById("q3Img2").src="../img/solution/q3_10.png";
 });
-$("#q2Item2").mouseleave(function(){
-    document.getElementById("q2Img2").src="../img/solution/q2_2.png";
+$("#q3Item2").mouseleave(function(){
+    document.getElementById("q3Img2").src="../img/solution/q3_2.png";
 });
-$("#q2Item3").mouseenter(function(){
-    document.getElementById("q2Img3").src="../img/solution/q2_11.png";
+$("#q3Item3").mouseenter(function(){
+    document.getElementById("q3Img3").src="../img/solution/q3_11.png";
 });
-$("#q2Item3").mouseleave(function(){
-    document.getElementById("q2Img3").src="../img/solution/q2_3.png";
+$("#q3Item3").mouseleave(function(){
+    document.getElementById("q3Img3").src="../img/solution/q3_3.png";
 });
-$("#q2Item4").mouseenter(function(){
-    document.getElementById("q2Img4").src="../img/solution/q2_12.png";
+$("#q3Item4").mouseenter(function(){
+    document.getElementById("q3Img4").src="../img/solution/q3_12.png";
 });
-$("#q2Item4").mouseleave(function(){
-    document.getElementById("q2Img4").src="../img/solution/q2_4.png";
+$("#q3Item4").mouseleave(function(){
+    document.getElementById("q3Img4").src="../img/solution/q3_4.png";
 });
-$("#q2Item5").mouseenter(function(){
-    document.getElementById("q2Img5").src="../img/solution/q2_13.png";
+$("#q3Item5").mouseenter(function(){
+    document.getElementById("q3Img5").src="../img/solution/q3_13.png";
 });
-$("#q2Item5").mouseleave(function(){
-    document.getElementById("q2Img5").src="../img/solution/q2_5.png";
+$("#q3Item5").mouseleave(function(){
+    document.getElementById("q3Img5").src="../img/solution/q3_5.png";
 });
-$("#q2Item6").mouseenter(function(){
-    document.getElementById("q2Img6").src="../img/solution/q2_14.png";
+$("#q3Item6").mouseenter(function(){
+    document.getElementById("q3Img6").src="../img/solution/q3_14.png";
 });
-$("#q2Item6").mouseleave(function(){
-    document.getElementById("q2Img6").src="../img/solution/q2_6.png";
+$("#q3Item6").mouseleave(function(){
+    document.getElementById("q3Img6").src="../img/solution/q3_6.png";
 });
-$("#q2Item7").mouseenter(function(){
-    document.getElementById("q2Img7").src="../img/solution/q2_15.png";
+$("#q3Item7").mouseenter(function(){
+    document.getElementById("q3Img7").src="../img/solution/q3_15.png";
 });
-$("#q2Item7").mouseleave(function(){
-    document.getElementById("q2Img7").src="../img/solution/q2_7.png";
+$("#q3Item7").mouseleave(function(){
+    document.getElementById("q3Img7").src="../img/solution/q3_7.png";
 });
-$("#q2Item8").mouseenter(function(){
-    document.getElementById("q2Img8").src="../img/solution/q2_16.png";
+$("#q3Item8").mouseenter(function(){
+    document.getElementById("q3Img8").src="../img/solution/q3_16.png";
 });
-$("#q2Item8").mouseleave(function(){
-    document.getElementById("q2Img8").src="../img/solution/q2_8.png";
+$("#q3Item8").mouseleave(function(){
+    document.getElementById("q3Img8").src="../img/solution/q3_8.png";
 });
