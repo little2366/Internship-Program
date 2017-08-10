faae121cc5c0169dd781e60e710529edfb0be0cc
little2366 <466707620@qq.com>
2017-08-10 17:26:51 +0800

    8/10

--- handesoft/js/index.js
+++ handesoft/js/index.js
@@ -131,3 +131,19 @@ $('#carousel ul').carouFredSel({
     pagination: "#pager",
     scroll: 1000
 });
+
+var bannerSlider = new Slider($('#banner_tabs'), {
+    time: 5000,
+    delay: 400,
+    event: 'hover',
+    auto: true,
+    mode: 'fade',
+    controller: $('#bannerCtrl'),
+    activeControllerCls: 'active'
+});
+$('#banner_tabs .flex-prev').click(function() {
+    bannerSlider.prev()
+});
+$('#banner_tabs .flex-next').click(function() {
+    bannerSlider.next()
+});
