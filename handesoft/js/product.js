827624ba39f84a38f04d4a1549d997e03d35c4c8
little2366 <466707620@qq.com>
2017-08-13 20:28:40 +0800

    8/13

--- handesoft/js/product.js
+++ handesoft/js/product.js
@@ -2,3 +2,11 @@ $(".q2LCItem").on("click",function(){
     $(this).addClass("itemActive");
     $(this).siblings().removeClass("itemActive");
 });
+
+$(".q2LCItem").on("click",function(){
+    var id = $(this).attr("id");
+    console.log(id);
+    var title_id = "#q2LTitle" + id;
+    $(title_id).css("display","block");
+    $(title_id).siblings(".q2LTitle").css("display","none");
+});
